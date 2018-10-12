b29b366abbcc110e092aa3672c71bd2da9329f6f
adsamcik <adsamcik@users.noreply.github.com>
2018-10-12 17:25:29 +0200

    Updated Rgba to use unsigned int

--- src/utility/Rgba.kt
+++ src/utility/Rgba.kt
@@ -1,53 +1,62 @@
 package utility
 
-data class Rgba(var value: Int) {
+@ExperimentalUnsignedTypes
+data class Rgba(var value: UInt) {
 
-	constructor(red: Int, green: Int, blue: Int, alpha: Int = 255) : this(
-			red.and(255).shl(24) +
-					green.and(255).shl(16) +
-					blue.and(255).shl(8) +
-					alpha.and(255)
+	constructor(red: UInt, green: UInt, blue: UInt, alpha: UInt = 255U) : this(
+			(red.and(255U).shl(24) +
+					green.and(255U).shl(16) +
+					blue.and(255U).shl(8) +
+					alpha.and(255U)).toUInt()
 	)
+
+	constructor(red: Int, green: Int, blue: Int, alpha: Int = 255) : this(red.toUInt(), green.toUInt(), blue.toUInt(), alpha.toUInt())
 
 	var red
 		get() = value.shr(24)
 		set(value) = setColorChannel(value, 24)
 
 	var green
-		get() = value.shr(16).and(255)
+		get() = value.shr(16).and(255U)
 		set(value) = setColorChannel(value, 16)
 
 	var blue
-		get() = value.shr(8).and(255)
+		get() = value.shr(8).and(255U)
 		set(value) = setColorChannel(value, 8)
 
 	var alpha
-		get() = value.and(255)
+		get() = value.and(255U)
 		set(value) = setColorChannel(value, 0)
 
 	val rgbaString: String
 		get() = "rgba($red,$green,$blue,$alpha)"
 
 
-	private fun setColorChannel(value: Int, offset: Int) {
+	private fun setColorChannel(value: UInt, offset: Int) {
 		if (offset > 24 || offset < 0 || offset.rem(8) != 0)
 			throw IllegalArgumentException("Offset must be 0, 8, 16 or 24. Was $offset")
 
-		val channelMask = 255.shl(offset)
+		val channelMask = 255U.shl(offset)
 
-		this.value = this.value.and(channelMask.inv()).or(value.toInt().shl(offset))
+		this.value = this.value.and(channelMask.inv()).or(value.shl(offset))
 	}
 
 
 	companion object {
 		val RED
-			get() = Rgba(255, 0, 0, 255)
+			get() = Rgba(255, 0, 0)
 
 		val GREEN
-			get() = Rgba(0, 255, 0, 255)
+			get() = Rgba(0, 255, 0)
 
 		val BLUE
-			get() = Rgba(0, 0, 255, 255)
+			get() = Rgba(0, 0, 255)
+
+		val BLACK
+			get() = Rgba(0, 0, 0)
+
+		val WHITE
+			get() = Rgba(255, 255, 255)
 	}
 
 }
