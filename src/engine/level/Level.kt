4f0eb07eafdfa1e1c61a8c4f315abe4dbcd44c03
adsamcik <adsamcik@users.noreply.github.com>
2018-10-19 18:10:25 +0200

    WIP Levels

--- src/engine/level/Level.kt
+++ src/engine/level/Level.kt
@@ -2,8 +2,23 @@ package engine.level
 
 import engine.entity.Entity
 
-class Level {
+class Level(val id: String) {
 	private val staticEntities = mutableListOf<Entity>()
 	private val dynamicEntities = mutableListOf<Entity>()
 	private val playerEntities = mutableListOf<Entity>()
+
+	fun addStaticEntity(entity: Entity) {
+		staticEntities.add(entity)
+	}
+
+	fun addDynamicEntity(entity: Entity) {
+		dynamicEntities.add(entity)
+	}
+
+	fun toJson() = toJson(this)
+
+	companion object {
+		fun toJson(level: Level) = JSON.stringify(level)
+		fun fromJson(json: String) = JSON.parse<Level>(json)
+	}
 }
