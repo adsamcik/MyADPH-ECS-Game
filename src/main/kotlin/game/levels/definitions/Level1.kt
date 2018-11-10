a07caa88db324c0b4105cbce65b25657e84934e9
adsamcik <adsamcik@users.noreply.github.com>
2018-11-10 21:38:45 +0100

    Set energy to full

--- src/main/kotlin/game/levels/definitions/Level1.kt
+++ src/main/kotlin/game/levels/definitions/Level1.kt
@@ -57,7 +57,7 @@ class Level1 : Level("level1") {
 			setPlayer(true)
 			setReceiveModifiers(true)
 			setFollow(true)
-			addComponent { EnergyComponent(0.0, 70.0, 150.0, 100.0) }
+			addComponent { EnergyComponent(100.0, 70.0, 150.0, 100.0) }
 		}
 	}
 
