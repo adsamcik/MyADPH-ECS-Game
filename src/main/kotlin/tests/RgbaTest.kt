a9c4412cd8f6d7a1e94eb167d89ce804d72d0ce9
adsamcik <adsamcik@users.noreply.github.com>
2018-11-14 18:01:03 +0100

    Refactored TestRunner, Add RgbaTests

---
+++ src/main/kotlin/tests/RgbaTest.kt
@@ -0,0 +1,26 @@
+package tests
+
+import utility.Assert
+import utility.Rgba
+
+class RgbaTest : ITest {
+	override fun run() {
+		baseTest()
+		randomTest()
+	}
+
+	private fun baseTest() {
+		Assert.equals(0xFF0000, Rgba.RED.rgb)
+		Assert.equals(0x00FF00, Rgba.GREEN.rgb)
+		Assert.equals(0x0000FF, Rgba.BLUE.rgb)
+	}
+
+	private fun randomTest() {
+		val rng = kotlin.random.Random(0xF0F0F0)
+		for (i in 0..10000) {
+			val value = rng.nextInt()
+			val color = Rgba(value)
+			Assert.equals(value, color.rgba)
+		}
+	}
+}
--- src/main/kotlin/tests/TestRunner.kt
+++ src/main/kotlin/tests/TestRunner.kt
@@ -1,7 +1,20 @@
 package tests
 
-object TestRunner {
+class TestRunner {
+	val testList = mutableListOf<ITest>()
+
+	init {
+		testList.add(Double2Test())
+		testList.add(RgbaTest())
+	}
+
+
 	fun run() {
-		Double2Test().run()
+		console.log("Running tests")
+		testList.forEach {
+			console.log("Running ${it::class.simpleName}")
+			it.run()
+		}
+		console.log("All tests ran successfully")
 	}
 }
