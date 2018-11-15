eff76f7a34834cc33d3780a8026efe4cec2a376e
adsamcik <adsamcik@users.noreply.github.com>
2018-11-15 16:52:21 +0100

    Fixed radians calculation

--- src/main/kotlin/extensions/MathExtensions.kt
+++ src/main/kotlin/extensions/MathExtensions.kt
@@ -1,9 +1,10 @@
 package extensions
 
 object MathExtensions {
-	private const val DEG_TO_RAD = kotlin.math.PI / 360.0
+	private const val DEG_TO_RAD = kotlin.math.PI / 180.0
+	private const val RAD_TO_DEG = 180.0 / kotlin.math.PI
 
 	fun toRadians(degrees: Double) = degrees * DEG_TO_RAD
 
-	fun toDegrees(radians: Double) = radians / kotlin.math.PI * 360
+	fun toDegrees(radians: Double) = radians * RAD_TO_DEG
 }
