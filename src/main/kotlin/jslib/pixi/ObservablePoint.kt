6538f5b0851749d396872e89aece606a3fa1499a
adsamcik <adsamcik@users.noreply.github.com>
2019-09-26 19:33:06 +0200

    Updated pixi definitions

--- src/main/kotlin/jslib/pixi/ObservablePoint.kt
+++ src/main/kotlin/jslib/pixi/ObservablePoint.kt
@@ -3,4 +3,22 @@
 
 package jslib.pixi
 
-open external class ObservablePoint : jslib.pixi.Point
+open external class ObservablePoint(
+	callback: () -> Unit,
+	scope: Any,
+	x: Number = definedExternally,
+	y: Number = definedExternally
+) : IPoint {
+	override var x: Double
+	override var y: Double
+
+	fun clone(): ObservablePoint
+
+	override fun copyFrom(observablePoint: IPoint)
+	override fun copyTo(observablePoint: IPoint)
+
+	override fun set(x: Number, y: Number)
+
+	@Suppress("CovariantEquals")
+	override fun equals(point: IPoint): Boolean
+}
