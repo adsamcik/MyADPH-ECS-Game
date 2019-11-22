fc7b96fd53fda5d44395fcd02b69a3757fc2bddd
adsamcik <adsamcik@users.noreply.github.com>
2019-11-22 17:13:27 +0100

    Update CheckpointComponent.kt

--- src/main/kotlin/ecs/components/triggers/CheckpointComponent.kt
+++ src/main/kotlin/ecs/components/triggers/CheckpointComponent.kt
@@ -2,9 +2,14 @@ package ecs.components.triggers
 
 import engine.component.IComponent
 import general.Double2
+import kotlinx.serialization.SerialInfo
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class CheckpointComponent(val id: Int, val respawnPosition: Double2, val type: CheckpointType, var isVisited: Boolean = false) : IComponent
 
+@Serializable
 enum class CheckpointType {
 	Start,
 	Standard,
