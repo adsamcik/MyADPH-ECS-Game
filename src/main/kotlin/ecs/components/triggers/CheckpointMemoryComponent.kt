552f9f7a7dfaee71afb5842206bccd0303e3f3e5
adsamcik <adsamcik@users.noreply.github.com>
2019-12-01 12:48:42 +0100

    Marked Checkpoint memory properly as generated component

--- src/main/kotlin/ecs/components/triggers/CheckpointMemoryComponent.kt
+++ src/main/kotlin/ecs/components/triggers/CheckpointMemoryComponent.kt
@@ -1,9 +1,9 @@
 package ecs.components.triggers
 
-import engine.component.IComponent
+import engine.component.IGeneratedComponent
 
 data class CheckpointMemoryComponent(
 	var lastCheckpoint: CheckpointComponent,
 	val checkpointsTotal: Int,
 	var checkpointsVisited: Int = 0
-) : IComponent
+) : IGeneratedComponent
